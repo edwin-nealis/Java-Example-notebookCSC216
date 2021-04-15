0269eb563563030d8a2c853ba9b68760df10aeb9
Edwin Nealis <ewnealis@ncsu.edu>
2021-04-14 20:07:16 -0400

    started implementation for reader and wrote all tests for reader

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReader.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -9,10 +10,13 @@ import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.AbstractTaskList;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.Task;
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
+
 /**
- * class contains a method and two helper methods that take a file and return a notebook as described in the file.
- * class also has a constructor which does nothing.
- * @author edwin 
+ * class contains a method and two helper methods that take a file and return a
+ * notebook as described in the file. class also has a constructor which does
+ * nothing.
+ * 
+ * @author edwin
  *
  */
 public class NotebookReader {
@@ -22,29 +26,100 @@ public class NotebookReader {
 	public NotebookReader() {
 		// left blank on purpose
 	}
-	
+
 	/**
 	 * method takes a file as input and returns a notebook with tasklist and task
-	 * specified by the file uses two helper methods in the creation of the notebook. 
+	 * specified by the file uses two helper methods in the creation of the
+	 * notebook.
+	 * 
 	 * @param file file to be processed
 	 * @return notebook that is outlined in file
 	 */
 	public static Notebook readNodebookFile(File file) {
-		return null;
+		String notebookName;
+		String inputString = "";
+		String taskListToken = "";
+		Scanner input;
+		Notebook nb = null;
+		try {
+			input = new Scanner(file);
+			while (input.hasNext()) {
+				inputString += input.nextLine() + "\n";
+			}
+			input.close();
+			if (!inputString.substring(0, 1).equals("!")) {
+				throw new IllegalArgumentException("Unable to load file.");
+			}
+			Scanner taskList = new Scanner(inputString);
+			taskList.useDelimiter("\\r?\\n?[#]");
+			notebookName = taskList.next();
+			notebookName = notebookName.substring(1);
+			notebookName = notebookName.trim();
+			nb = new Notebook(notebookName);
+
+			while (taskList.hasNext()) {
+				try {
+					taskListToken = taskList.next();
+					TaskList tl = processTaskList(taskListToken);
+					nb.addTaskList(tl);
+				} catch (IllegalArgumentException e) {
+					// blank on propose
+				}
+			}
+			taskList.close();
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Unable to load file.");
+		}
+		return nb;
 	}
+
 	/**
 	 * helper method that processes a string containing a task list
+	 * 
 	 * @param s string to be processed
 	 * @return TaskList created from string
+	 * @throws IllegalArgumentException if string can not be processed
 	 */
 	private static TaskList processTaskList(String s) {
-		return null;
+		String task;
+		String taskListName;
+		int completedCount;
+		Scanner in = new Scanner(s);
+		in.useDelimiter("\\r?\\n?[*]");
+		String temp = in.next();
+		Scanner taskList = new Scanner(temp);
+		taskList.useDelimiter(",");
+		try {
+		taskListName = taskList.next();
+		taskListName = taskListName.substring(1);
+		taskListName = taskListName.trim();
+		completedCount = taskList.nextInt();
+		taskList.close();
 	}
-	
+		catch (NoSuchElementException e) {
+			in.close();
+			throw new IllegalArgumentException();
+		}
+		TaskList tl = new TaskList(taskListName, completedCount);
+			while (in.hasNext()) {
+				try {
+					task = in.next();
+				task = task.trim();
+				Task t = processTask(tl, s);
+				tl.addTask(t);
+		} catch (IllegalArgumentException e) {
+			// blank on purpose
+		}
+			}
+			in.close();
+			return tl;
+	}
+
 	/**
 	 * helper method that processes a string to get a task
+	 * 
 	 * @param ats list to add task too
-	 * @param s string to be processed
+	 * @param s   string to be processed
 	 * @return Task created from string
 	 */
 	private static Task processTask(AbstractTaskList ats, String s) {
@@ -54,6 +129,7 @@ public class NotebookReader {
 		boolean active = false;
 		boolean recurring = false;
 		Scanner in = new Scanner(s);
+		in.useDelimiter("\\r?\\n?");
 		try {
 			firstLine = in.nextLine();
 			while (in.hasNext()) {
@@ -65,25 +141,26 @@ public class NotebookReader {
 				String temp = task.next();
 				if (temp.equals("recurring")) {
 					recurring = true;
-				}
-				else if (temp.equals("active")) {
+				} else if (temp.equals("active")) {
 					active = true;
-				}
-				else taskName = temp;
+				} else
+					taskName = temp;
 			}
 			task.close();
-			
+
 		} catch (InputMismatchException e) {
-		in.close();
-		throw new IllegalArgumentException();
+			in.close();
+			throw new IllegalArgumentException();
 		} catch (NoSuchElementException e) {
-		in.close();
-		throw new IllegalArgumentException();
-	}
+			in.close();
+			throw new IllegalArgumentException();
+		}
+		taskName = taskName.substring(1);
+		taskName = taskName.trim();
 		Task task = new Task(taskName, taskDescription, recurring, active);
 		task.addTaskList(ats);
 		in.close();
-		return null;
+		return task;
 	}
 
 }
--- Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReaderTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tasks/model/io/NotebookReaderTest.java
@@ -13,19 +13,129 @@ import edu.ncsu.csc216.wolf_tasks.model.notebook.Notebook;
  *
  */
 public class NotebookReaderTest {
-	/** valid tests file patch */
-	private File file = new File("test-file/notebook1.txt");
+	/** valid tests file full */
+	private File file1 = new File("test-files/notebook1.txt");
+	/** holds a test file with a notebook with just a name */
+	private File file2 = new File("test-files/notebook0.txt");
+	/** valid test file items missing */
+	private File file3 = new File("test-files/notebook2.txt");
+	/** invalid test file missing ! */
+	private File file4 = new File("test-files/notebook3.txt");
+	/** valid test file missing completed task info */
+	private File file5 = new File("test-files/notebook4.txt");
+	/** valid test file missing task list names */
+	private File file6 = new File("test-files/notebook5.txt");
+	/** valid test file negative completed tasks */
+	private File file7 = new File("test-files/notebook6.txt");
+	/** valid test file missing task name */
+	private File file8 = new File("test-files/notebook7.txt");
 	/** holds array of taskLists names in array form*/
 	String[] taskLists = new String[] { "CSC 216", "CSC 226", "Habits" };
+	/** holds expected task values for CSCS 216*/
+	String[][] array = new String[][] { { "CSC 216", "Read Project 2 requirements"}, 
+		{ "CSC 216", "Create CRC Cards to UMLetino" }, { "CSC 216", "Transfer CRC Cards to UMLetino" },
+		{ "CSC 216", "Download design proposal and rational template" }, { "CSC 216", "Write design proposal and rationale" },
+		{ "CSC 216", "Identify 5 system tests" }, { "CSC 216", "Watch lecture video" },
+		{ "CSC 216", "Complete exercises" }, { "CSC 216", "Complete quizzes" } };
+	/** holds the expected task values for CSC 226 */ 
+	String [][] array2 = new String[][] { { "CSC 226", "Homework 7" }, { "CSC 226", "Homework 8" }, { "CSC 226", "Homework 9" }, 
+		{ "CSC 226", "Homework 10" }, { "CSC 226", "Watch lectures" } }; 
+	/** holds the expected task values for Habits */
+	String[][] array3 = new String[][] { { "Habits", "Excercise" }, { "Habits", "Floss" } };
+		
 
+	/** 
+	 * tests the note book reader from file
+	 */
 	@Test
-	public void testReadNoteBookFile() {
-		Notebook nb = NotebookReader.readNodebookFile(file);
+	public void testReadNoteBookFileValid() {
+		Notebook nb = NotebookReader.readNodebookFile(file1);
+		Notebook nb2 = NotebookReader.readNodebookFile(file2);
+		Notebook nb3 = NotebookReader.readNodebookFile(file3);
+		Notebook nb4 = NotebookReader.readNodebookFile(file5);
+		Notebook nb5 = NotebookReader.readNodebookFile(file6);
+		Notebook nb6 = NotebookReader.readNodebookFile(file7);
+		Notebook nb7 = NotebookReader.readNodebookFile(file8);
+		assertEquals(nb2.getNotebookName(), "Summer Plans");
+		assertEquals(nb2.getTaskListsNames().length, 1);
+		assertEquals(nb4.getNotebookName(), "Personal");
+		assertEquals(nb4.getTaskListsNames().length, 1);
+		assertEquals(nb5.getNotebookName(), "Personal");
+		assertEquals(nb5.getTaskListsNames().length, 1);
+		assertEquals(nb6.getNotebookName(), "Personal");
+		assertEquals(nb6.getTaskListsNames().length, 1);
+		
+		assertEquals(nb7.getNotebookName(), "Personal");
+		assertEquals(nb7.getTaskListsNames()[1], "Habits");
+		nb7.setCurrentTaskList("Habits");
+		assertEquals(nb7.getCurrentTaskList().getCompletedCount(), 3);
+		assertEquals(nb7.getCurrentTaskList().getTasksAsArray()[0][0], "Habits");
+		assertEquals(nb7.getCurrentTaskList().getTasksAsArray()[0][1], "Floss");
+		assertEquals(nb7.getCurrentTaskList().getTasks().size(), 1);
+
+		nb.setCurrentTaskList("CSC 16");
 		assertEquals(nb.getNotebookName(), "School");
-		assertEquals(nb.getTaskListsNames()[0],taskLists[0]);
-		assertEquals(nb.getTaskListsNames()[1],taskLists[1]);
-		assertEquals(nb.getTaskListsNames()[2],taskLists[2]);
-		nb.getCurrentTaskList().getTasksAsArray();
+		assertEquals(nb.getTaskListsNames()[1], taskLists[0]);
+		assertEquals(nb.getTaskListsNames()[2], taskLists[1]);
+		assertEquals(nb.getTaskListsNames()[3], taskLists[2]);
+		assertEquals(nb.getCurrentTaskList().getTasks().size(), 9);
+		assertEquals(nb.getCurrentTaskList().getCompletedCount(), 35);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[0][0], array[0][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[0][1], array[0][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[1][0], array[1][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[1][1], array[1][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[2][0], array[2][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[2][1], array[2][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[3][0], array[3][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[3][1], array[3][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[4][0], array[4][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[4][1], array[4][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[5][0], array[5][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[5][1], array[5][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[6][0], array[6][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[6][1], array[6][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[7][0], array[7][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[7][1], array[7][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[8][0], array[8][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[8][1], array[8][1]);
+		nb.setCurrentTaskList("CSC 226");
+		assertEquals(nb.getCurrentTaskList().getTasks().size(), 6);
+		assertEquals(nb.getCurrentTaskList().getCompletedCount(), 23);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[0][0], array2[0][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[0][1], array2[0][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[1][0], array2[1][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[1][1], array2[1][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[2][0], array2[2][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[2][1], array2[2][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[3][0], array2[3][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[3][1], array2[3][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[4][0], array2[4][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[4][1], array2[4][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[5][0], array2[5][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[5][1], array2[5][1]);
+		nb.setCurrentTaskList("Habits");
+		assertEquals(nb.getCurrentTaskList().getTasks().size(), 2);
+		assertEquals(nb.getCurrentTaskList().getCompletedCount(), 0);
+		assertEquals(nb.getCurrentTaskList().getCompletedCount(), 35);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[0][0], array3[0][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[0][1], array3[0][1]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[1][0], array3[1][0]);
+		assertEquals(nb.getCurrentTaskList().getTasksAsArray()[1][1], array3[1][1]);
+		
+	}
+	/**
+	 * tests read from file with invalid file
+	 */
+	@Test
+	public void testInvalidFiles() {
+		Notebook nb = null;
+		try {
+			nb = NotebookReader.readNodebookFile(file4);
+			fail();
+		}
+		catch (IllegalArgumentException e) {
+			assertNull(nb);
+		}
 	}
 
 }
